dc5c97122d65431dbdc443de2fff97f622691b7d
tallsong <a1249812431@gmail.com>
2021-03-12 19:56:41 +0800

    implement main window

--- mainscene.cpp
+++ mainscene.cpp
@@ -6,6 +6,12 @@ MainScene::MainScene(QWidget *parent)
     , ui(new Ui::MainScene)
 {
     ui->setupUi(this);
+    this->setWindowTitle("CoinFilp");
+    this->setFixedSize(300,528);
+    this->setWindowIcon(QIcon{":/img/Coin0001.png"});
+    connect(ui->actionquit,&QAction::triggered,[=](){
+        this->close();
+    });
 }
 
 MainScene::~MainScene()
